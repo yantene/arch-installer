68f8cab331af5768e7119806b0461914a7c2ba17
yantene <contact@yantene.net>
2016-10-01 11:28:59 +0900

    EFI 機向けのスクリプトで書き換えた部分を総じて同じようにした

--- installer.sh
+++ installer.sh
@@ -11,7 +11,10 @@ read -p  'hostname (YanteneLaptop): ' HOSTNAME
 read -p  'username (yantene): ' USERNAME
 [[ -z $USERNAME ]] && USERNAME='yantene'
 
-read -sp 'password: ' PASSWORD
+while [[ -z $PASSWORD ]]; do
+  read -sp 'password: ' PASSWORD
+  echo
+done
 
 set -ux
 
@@ -28,7 +31,7 @@ n
 +2M
 ef02
 c
-bios_boot_partition
+bios_boot
 n
 
 
@@ -36,51 +39,74 @@ n
 8300
 c
 2
-linux_btrfs_partition
+linux_root
 w
 y
 EOS
 
-## get each device file names
+## set each device file names
 
-parts=(`fdisk -l $DEVICE | tail -n 2 | cut -d' ' -f1`)
-part1=${parts[0]}
-part2=${parts[1]}
+bios_boot='/dev/disk/by-partlabel/bios_boot'
+linux_root='/dev/disk/by-partlabel/linux_root'
+
+while [[ ! -e $bios_boot ]] ||
+      [[ ! -e $linux_root ]]; do
+  sleep 0.1
+done
 
 ## format
 
-mkfs.btrfs -L LINUX_BTRFS -f $part2
+mkfs.btrfs -f -L LINUX_ROOT -f $linux_root
+
+## set device mount options
+
+linux_root_mntopts='rw,noatime,discard,ssd,autodefrag,compress=lzo,space_cache'
 
 ## create btrfs subvolume
 
-mount $part2 /mnt
+mount -o $linux_root_mntopts $linux_root /mnt
 cd /mnt
 btrfs subvolume create root
-rootvol_id=`btrfs subvol list -p . | cut -d' ' -f2`
+btrfs subvolume set-default `btrfs subvol list -p . | cut -d' ' -f2` .
 btrfs subvolume create root/home
-btrfs subvolume set-default $rootvol_id .
-cd -
+btrfs subvolume create snapshots
+cd ~
 umount /mnt
 
 ## mount
 
-btrfs_mntopts='noatime,discard,ssd,autodefrag,compress=lzo,space_cache'
-mount -o $btrfs_mntopts $part2 /mnt
+mount -o $linux_root_mntopts $linux_root /mnt
+mkdir /mnt/boot
 
 # INSTALL
 
 ## mirror server
 cat > /etc/pacman.d/mirrorlist <<EOS
 Server = http://ftp.jaist.ac.jp/pub/Linux/ArchLinux/\$repo/os/\$arch
+Server = http://ftp.nara.wide.ad.jp/pub/Linux/archlinux/\$repo/os/\$arch
 Server = http://ftp.tsukuba.wide.ad.jp/Linux/archlinux/\$repo/os/\$arch
-Server = http://archlinux.cs.nctu.edu.tw/\$repo/os/\$arch
+Server = http://srv2.ftp.ne.jp/Linux/packages/archlinux/\$repo/os/\$arch
+Server = http://mirror.premi.st/archlinux/\$repo/os/\$arch
 Server = http://ftp.tku.edu.tw/Linux/ArchLinux/\$repo/os/\$arch
+Server = http://mirrors.163.com/archlinux/\$repo/os/\$arch
+Server = http://ftp.kaist.ac.kr/ArchLinux/\$repo/os/\$arch
+Server = http://mirrors.xjtu.edu.cn/archlinux/\$repo/os/\$arch
 Server = http://shadow.ind.ntou.edu.tw/archlinux/\$repo/os/\$arch
-Server = http://mirrors.cdndepo.com/archlinux/\$repo/os/\$arch
-Server = http://mirror.pregi.net/pub/Linux/archlinux/\$repo/os/\$arch
-Server = http://mirror.rackspace.com/archlinux/\$repo/os/\$arch
+Server = https://mirrors.xjtu.edu.cn/archlinux/\$repo/os/\$arch
+Server = https://mirrors.ustc.edu.cn/archlinux/\$repo/os/\$arch
+Server = http://ftp.kddilabs.jp/Linux/packages/archlinux/\$repo/os/\$arch
+Server = http://archlinux.cs.nctu.edu.tw/\$repo/os/\$arch
+Server = http://mirrors.zju.edu.cn/archlinux/\$repo/os/\$arch
+Server = http://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch
 Server = http://mirrors.ustc.edu.cn/archlinux/\$repo/os/\$arch
+Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch
+Server = http://run.hit.edu.cn/archlinux/\$repo/os/\$arch
+Server = http://mirror-fpt-telecom.fpt.net/archlinux/\$repo/os/\$arch
+Server = http://mirrors.cug.edu.cn/archlinux/\$repo/os/\$arch
 Server = http://ftp.yzu.edu.tw/Linux/archlinux/\$repo/os/\$arch
+Server = http://mirrors.neusoft.edu.cn/archlinux/\$repo/os/\$arch
+Server = http://f.archlinuxvn.org/archlinux/\$repo/os/\$arch
+Server = http://mirrors.cqu.edu.cn/archlinux/\$repo/os/\$arch
 EOS
 
 ## install
@@ -103,7 +129,7 @@ CHROOT="arch-chroot /mnt"
 ## edit fstab
 
 cat > /mnt/etc/fstab <<EOS
-PARTLABEL='linux_btrfs_partition' /     btrfs rw,noatime,compress=lzo,ssd,discard,space_cache,autodefrag,subvolid=257,subvol=/root,subvol=root     0 0
+PARTLABEL='linux_root'  /     btrfs $linux_root_mntopts 0 0
 EOS
 
 ## hostname
@@ -112,8 +138,10 @@ echo $HOSTNAME > /mnt/etc/hostname
 
 ## locale
 
-$CHROOT sed -i -e 's/#ja_JP.UTF-8/ja_JP.UTF-8/' /etc/locale.gen
-$CHROOT sed -i -e 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+cat > /mnt/etc/locale.gen <<EOS
+ja_JP.UTF-8 UTF-8
+en_US.UTF-8 UTF-8
+EOS
 echo 'LANG=ja_JP.UTF-8' > /mnt/etc/locale.conf
 $CHROOT locale-gen
 echo -e 'KEYMAP=us\nFONT=Lat2-Terminus16' > /mnt/etc/vconsole.conf
@@ -127,8 +155,6 @@ $CHROOT pacman --noconfirm -S grub
 $CHROOT grub-install --recheck --target=i386-pc $DEVICE
 $CHROOT grub-mkconfig -o /boot/grub/grub.cfg
 
-sleep 10
-
 ## pacman settings
 
 ### multilib
@@ -140,15 +166,6 @@ Include = /etc/pacman.d/mirrorlist
 EOS
 pacman -Syy
 fi
-
-### yaourt
-
-cat >> /mnt/etc/pacman.conf <<EOS
-[archlinuxfr]
-SigLevel = Never
-Server = http://repo.archlinux.fr/\$arch
-EOS
-$CHROOT pacman -Sy --noconfirm archlinuxfr/yaourt
 
 ## root settings
 
@@ -169,10 +186,13 @@ $PASSWORD
 EOS
 $CHROOT sed -i 's/^#\s%wheel\s*ALL=(ALL)\s*ALL$/%wheel\tALL=(ALL)\tALL/g' /etc/sudoers
 
-set +x
+## create btrfs snapshot
 
-echo 'TODO: '
-echo ' 1. Edit kernel parameters (/mnt/boot/loader/entries/arch.conf)'
-echo ' 2. Check fstab (/mnt/etc/fstab)'
-echo ' 3. `umount -R /mnt`'
-echo ' 4. `reboot`'
+umount -R /mnt
+mount -o $linux_root_mntopts,subvol=/ $linux_root /mnt
+ptime=`date +'%s'`
+btrfs subvolume snapshot /mnt/root      /mnt/snapshots/${ptime}-root
+btrfs subvolume snapshot /mnt/root/home /mnt/snapshots/${ptime}-home
+umount /mnt
+
+set +x
